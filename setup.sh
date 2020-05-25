939627a747d0a705ac4891f5b96403c605ab077c
anBarot <barot.anne@gmail.com>
2020-05-25 18:41:49 +0200

    adding wordpress and nginx services

--- setup.sh
+++ setup.sh
@@ -2,26 +2,9 @@ minikube start
 
 eval $(minikube docker-env)
 
-#install nginx ingress controller
-
-git clone https://github.com/nginxinc/kubernetes-ingress/
-cd kubernetes-ingress/deployments
-kubectl apply -f common/ns-and-sa.yaml
-kubectl apply -f rbac/rbac.yaml
-kubectl apply -f common/default-server-secret.yaml
-kubectl apply -f common/nginx-config.yaml
-kubectl apply -f common/vs-definition.yaml
-kubectl apply -f common/vsr-definition.yaml
-kubectl apply -f common/ts-definition.yaml
-kubectl apply -f common/gc-definition.yaml
-kubectl apply -f common/global-configuration.yaml
-kubectl apply -f deployment/nginx-ingress.yaml
-kubectl apply -f daemon-set/nginx-ingress.yaml
-kubectl create -f service/nodeport.yaml
-
 docker build -t nginx-img srcs/nginx/.
-# docker build -t wordpress-img srcs/wordpress/.
-# docker build -t mysql-img srcs/mysql/.
+docker build -t wordpress-img srcs/wordpress/.
+docker build -t mysql-img srcs/mysql/.
 # docker build -t phpmyadmin-img srcs/phpmyadmin/.
 # docker build -t ftps-img srcs/FTPS/.
 # docker build -t grafana-img srcs/grafana/.
@@ -30,8 +13,3 @@ docker build -t nginx-img srcs/nginx/.
 minikube addons enable ingress
 
 kubectl apply -k srcs/.
-
-
-
-
-#eval $(minikube docker-env -u)
--- srcs/mysql/mysql_init.sh
+++ srcs/mysql/mysql_init.sh
@@ -1,10 +1,3 @@
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-
-mysqladmin -u root password $MYSQL_ROOT_PASSWORD
-
-echo "CREATE DATABASE wp_database;" | mysql -u root --default-character-set=utf8
-echo "CREATE USER $WP_USER@$WORDPRESS_DB_HOST IDENTIFIED BY $WORDPRESS_DB_PASSWORD;" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON wp_database.* TO 'wp_user'@'localhost';" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
-
-rm mysql_init.sh
+mysql_install_db --user=mysql --ldata=/var/lib/mysql
+/usr/bin/mysqld_safe --datadir=/var/lib/mysql
+/bin/sh
